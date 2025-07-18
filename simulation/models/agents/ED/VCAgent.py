9c72e536c996b10d085016532f6a0f7f20bd1a4e
Rising_Date <1773261467@qq.com>
2025-07-18 19:37:59 +0800

    更新VCAgent

--- simulation/models/agents/ED/VCAgent.py
+++ simulation/models/agents/ED/VCAgent.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from simulation.models.agents.LLMAgent import LLMAgent
 
 MODEL_LIST = {
@@ -20,12 +22,12 @@ class VariableControlAgent(LLMAgent):
         '''
         self.is_first = True
 
-    def VCAnalysis(self, req, influence_factor, response_var, ):
+    def VCAnalysis(self, req, influence_factor, response_var, formula):
         info_prompt = '''
             在本次需求分析中，用户提出的需求为：{req}
             影响因素为：{influence_factor}
             响应变量为：{response_var}
-            影响因素和响应变量之间的对应关系为：{}
+            影响因素和响应变量之间的对应关系为：{formula}
             - 你回复的格式均为json，但是包含两种情况：
             - 第一种：如果你认为前面解析的'影响因素'和'响应变量'以及二者之间的对应关系均合理，回复格式为：
                 "is_reasonable": 1。
@@ -34,39 +36,25 @@ class VariableControlAgent(LLMAgent):
                     list的内容为dict，dict举例如下："'foreign_policy': '外交政策'"。
             - 第二种：如果你认为前面解析的不合理，回复格式如下：
                 "is_reasonable": 0。
-                "influence_factor": 影响因素，即实验的自变量，能够尽可能全面的反应对实验的影响，'influence_factor'应该为一个list。\
+                "reason": 前面解析不合理的原因，'reason'是一个中文的str。
+                "influence_factor": 影响因素，即实验的自变量，能够尽可能全面的反应对实验的影响，'influence_factor'为一个list。\
                     'influence_factor'的内容全部为英文。
-                "response_var": 响应变量，即实验的因变量，能够客观的反映实验的结果，'response_var'应该为一个list，\
+                "response_var": 响应变量，即实验的因变量，能够客观的反映实验的结果，'response_var'为一个list，\
                     'response_var'的内容全部为英文。
                 "formula": 影响因素和响应变量之间的对应公式，每个响应变量都有一个确定的对应公式，公式以数学公式的形式给出，\
                     'formula'的格式为json，每个元素的key为响应变量的名称，value为影响因素组成的一个公式，'formula'的长度与'response_var'相同\
-                    'formula'的内容全部为英文
+                    'formula'的内容全部为英文。
                 "var_explain": 对变量的中文解释，包含新生成的影响因素和响应变量，'var_explain'的格式为json，包含'influence_factor'和'response_var'两个key，\
                     分别是对'影响因素'和'响应变量'的中文解释，'influence_factor'和'response_var'的内容均为list，\
                     list的内容为dict，dict举例如下："'foreign_policy': '外交政策'"。
-                
-                    
-                
-            - 当你回复时，你必须采取下面的json格式，除去goal使用中文回答外，其余请全部使用英文回答，英文变量无需给出中文的解释
-                "goal": 用户此次实验或假设的目的，'goal'的类型请尽量从'现象解释'，'趋势预测'，'策略优化'中进行选择，并对其进行简单的解释\
-                    'goal'是一个json，包含'category'和'explain'，'category'和'explain'请使用中文回答。
-                "influence_factor": 影响因素，即实验的自变量，能够尽可能全面的反应对实验的影响，'influence_factor'应该为一个list。\
-                    'influence_factor'的内容全部为英文。
-                "response_var": 响应变量，即实验的因变量，能够客观的反映实验的结果，'response_var'应该为一个list，\
-                    'response_var'的内容全部为英文。
-                "formula": 影响因素和响应变量之间的对应公式，每个响应变量都有一个确定的对应公式，公式以数学公式的形式给出，\
-                    'formula'的格式为json，每个元素的key为响应变量的名称，value为影响因素组成的一个公式，'formula'的长度与'response_var'相同\
-                    'formula'的内容全部为英文
-                "exp_params": 实验参数的格式为json，包含'exp_method'和'params'，\
-                    'exp_method'是实验方法，具体参考上面提到的实验设计方法，'exp_method'是一个字符串，\
-                    'params'是根据'exp_method'生成的实验参数，你需要根据实验方法生成多组实验，每组实验是影响因素的不同取值水平组合，请设置合理的实验组数。\
-                    'param'是一个json，而不是一个list，'param'中key的数量与影响因素相等，每个元素的key为影响因素的名称，value为影响因素的取值，所有影响因素的取值数量应该相同。\
-                    'exp_params'的内容全部为英文。
+                "formula_explain": 对公式的中文解释，说出你对构造每个公式的思考过程，'formula_explain'是一个json，key的形式为'formula1', 'formula2'....
+                    'formula_explain'的key数量需要和'formula'的长度相同。
                 '''
         param_dict = {
             'req': req,
             'influence_factor': influence_factor,
-            'response_var': response_var
+            'response_var': response_var,
+            'formula': formula
         }
         if self.llm_model in MODEL_LIST['think']:
             llm_response, think = self.get_response(info_prompt, input_param_dict=param_dict,
@@ -75,21 +63,23 @@ class VariableControlAgent(LLMAgent):
             llm_response = self.get_response(info_prompt, input_param_dict=param_dict,
                                              is_first_call=self.is_first)
         self.is_first = False
-        res = {
-            "goal": {},
-            "influence_factor": [],
-            "response_var": [],
-            "formula": [],
-            "exp_params": [],
-        }
+        res = {}
         try:
-            res = {
-                "goal": llm_response['goal'],
-                "influence_factor": llm_response['influence_factor'],
-                "response_var": llm_response['response_var'],
-                "formula": llm_response['formula'],
-                "exp_params": llm_response['exp_params']
-            }
+            if llm_response['is_reasonable'] == 1:
+                res = {
+                    "is_reasonable": 'raAgent解析内容合理',
+                    "var_explain": llm_response['var_explain'],
+                }
+            else:
+                res = {
+                    "is_reasonable": 'raAgent解析内容不合理',
+                    'reason': llm_response['reason'],
+                    'influence_factor': llm_response['influence_factor'],
+                    'response_var': llm_response['response_var'],
+                    'formula': llm_response['formula'],
+                    'var_explain': llm_response['var_explain'],
+                    'formula_explain': llm_response['formula_explain'],
+                }
         except Exception as e:
             print(e)
 
--- simulation/scene/powerGame/test/main.py
+++ simulation/scene/powerGame/test/main.py
@@ -1,20 +1,26 @@
 from simulation.models.agents.RA.RAAgent import RequirementAnalysisAgent
 from simulation.models.agents.RA.RAObserver import RequirementAnalysisObserver
-
+from simulation.models.agents.ED.VCAgent import VariableControlAgent
 
 MODEL_LIST = ['deepseek-r1:32b', 'deepseek-r1:32b-qwen-distill-q8_0', 'gpt-4o']
 
-
 if __name__ == '__main__':
-    ra = RequirementAnalysisAgent(llm_model=MODEL_LIST[2])
+    raAgent = RequirementAnalysisAgent(llm_model=MODEL_LIST[2])
     raObserver = RequirementAnalysisObserver(llm_model=MODEL_LIST[2])
     req = '我想要复现古巴导弹危机中美国和古巴在各个时间段的行为，分析什么因素对战争的走势影响最大'
     analysis_num = 1
     while True:
-        analysis_res = ra.requirement_analysis(req)
+        analysis_res = raAgent.requirement_analysis(req)
         print(analysis_res)
         is_analysis_format_true = raObserver.requirement_format_judge(analysis_res)
         if is_analysis_format_true:
             print(f'------需求解析成功------')
             break
         print(f'------需求解析错误({analysis_num})------')
+
+    vcAgent = VariableControlAgent(llm_model=MODEL_LIST[2])
+    vc_res = vcAgent.VCAnalysis(req=req,
+                                influence_factor=analysis_res['influence_factor'],
+                                response_var=analysis_res['response_var'],
+                                formula=analysis_res['formula'])
+    print('VC Response: ', vc_res)
