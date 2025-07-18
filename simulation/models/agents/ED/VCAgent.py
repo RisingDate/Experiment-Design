a5c562a684e8aeacaaf719ce0b57e80a2405e71e
Rising_Date <1773261467@qq.com>
2025-07-19 00:39:45 +0800

    VCAgent增加实验参数分析函数

--- simulation/models/agents/ED/VCAgent.py
+++ simulation/models/agents/ED/VCAgent.py
@@ -19,6 +19,11 @@ class VariableControlAgent(LLMAgent):
             我们简单的对'影响因素'和'响应变量'之间以数学公式的形式建立了一些对应关系。
             你的任务是分析这些'响应变量'和'影响因素'能否正确反映用户的需求以及它们二者之间的对应关系是否合理。
             不论是否合理，请给出你的理由，如果不合理，请重新设计'影响因素'和'响应变量'以及他们之间的对应关系。
+            注意：下面是你掌握的计算实验方法内容，请你在选择实验方法时尽可能的参考下面的内容：
+                计算实验方法可以分为确定性实验设计和非确定性实验设计，
+                确定性实验设计包括：'一次一因子设计'，'NK因子设计'，'网格设计'，'超饱和设计'，'均匀设计'，'正交设计'，'拉丁方设计'，
+                '分布因子设计'，'顺序分支设计'，'迭代分布析因设计'，'多步组筛选设计' 和 '托辛筛选设计'；
+                非确定性实验设计包括：'Monte Carlo'，'Markov-Chain Monte Carlo' 和 '重采样'三类方法
         '''
         self.is_first = True
 
@@ -31,12 +36,13 @@ class VariableControlAgent(LLMAgent):
             - 你回复的格式均为json，但是包含两种情况：
             - 第一种：如果你认为前面解析的'影响因素'和'响应变量'以及二者之间的对应关系均合理，回复格式为：
                 "is_reasonable": 1。
+                "reason": 参数解析合理的原因，'reason'是一个中文的str。
                 "var_explain": 对变量的中文解释，包含影响因素和响应变量，'var_explain'的格式为json，包含'influence_factor'和'response_var'两个key，\
                     分别是对'影响因素'和'响应变量'的中文解释，'influence_factor'和'response_var'的内容均为list，\
                     list的内容为dict，dict举例如下："'foreign_policy': '外交政策'"。
             - 第二种：如果你认为前面解析的不合理，回复格式如下：
                 "is_reasonable": 0。
-                "reason": 前面解析不合理的原因，'reason'是一个中文的str。
+                "reason": 参数解析不合理的原因，'reason'是一个中文的str。
                 "influence_factor": 影响因素，即实验的自变量，能够尽可能全面的反应对实验的影响，'influence_factor'为一个list。\
                     'influence_factor'的内容全部为英文。
                 "response_var": 响应变量，即实验的因变量，能够客观的反映实验的结果，'response_var'为一个list，\
@@ -68,6 +74,7 @@ class VariableControlAgent(LLMAgent):
             if llm_response['is_reasonable'] == 1:
                 res = {
                     "is_reasonable": 'raAgent解析内容合理',
+                    'reason': llm_response['reason'],
                     "var_explain": llm_response['var_explain'],
                 }
             else:
@@ -79,6 +86,58 @@ class VariableControlAgent(LLMAgent):
                     'formula': llm_response['formula'],
                     'var_explain': llm_response['var_explain'],
                     'formula_explain': llm_response['formula_explain'],
+                }
+        except Exception as e:
+            print(e)
+
+        return res
+
+    def VCExpParamAnalysis(self, req, influence_factor, response_var, formula, exp_params):
+        info_prompt = f'''
+            在本次需求分析中，用户提出的需求为：{req}
+            影响因素为：{influence_factor}
+            响应变量为：{response_var}
+            影响因素和响应变量之间的对应关系为：{formula}
+            为了更好的分析响应变量的变化情况，我们选择的实验方法以及为影响因素设置的参数分布为：{exp_params}
+            - 你回复的格式均为json，但是包含两种情况：
+            - 第一种：如果你认为实验方法的选择和参数的设置是合理的，回复格式为：
+                "is_reasonable": 1。
+                "reason": 合理的原因，'reason'是一个中文的str。
+            - 第二种：如果你认为不合理或者'exp_params'中的参数与'影响因素'不符，回复格式如下：
+                "is_reasonable": 0。
+                "reason": 不合理的原因，'reason'是一个中文的str。
+                "exp_params": 实验参数的格式为json，与传入的'exp_params'格式完全相同，包含'exp_method'和'params'，\
+                    'exp_method'是实验方法，具体参考上面提到的实验设计方法，'exp_method'是一个字符串，\
+                    'params'是根据'exp_method'生成的实验参数，你需要根据实验方法生成多组实验，每组实验是影响因素的不同取值水平组合，请设置合理的实验组数。\
+                    'param'是一个json，而不是一个list，'param'中key的数量与影响因素相等，每个元素的key为影响因素的名称，value为影响因素的取值，所有影响因素的取值数量应该相同。\
+                    'exp_params'的内容全部为英文。
+        '''
+        param_dict = {
+            'req': req,
+            'influence_factor': influence_factor,
+            'response_var': response_var,
+            'formula': formula,
+            'exp_params': exp_params
+        }
+        if self.llm_model in MODEL_LIST['think']:
+            llm_response, think = self.get_response(info_prompt, input_param_dict=param_dict,
+                                                    is_first_call=self.is_first)
+        else:
+            llm_response = self.get_response(info_prompt, input_param_dict=param_dict,
+                                             is_first_call=self.is_first)
+        self.is_first = False
+        res = {}
+        try:
+            if llm_response['is_reasonable'] == 1:
+                res = {
+                    "is_reasonable": '实验参数设置合理',
+                    'reason': llm_response['reason'],
+                }
+            else:
+                res = {
+                    "is_reasonable": '实验参数设置不合理',
+                    'reason': llm_response['reason'],
+                    'exp_params': llm_response['exp_params'],
                 }
         except Exception as e:
             print(e)
--- simulation/scene/powerGame/test/main.py
+++ simulation/scene/powerGame/test/main.py
@@ -42,33 +42,62 @@ def log_with_tag(message, tag='GENERAL', level: Literal['debug', 'info', 'warnin
 
 
 if __name__ == '__main__':
+    # 显示开始一次新的实验
     log_with_tag(message=' ', tag='New Exp', level='critical')
+    exp_param = {
+        'goal': None,
+        'influence_factor': None,
+        'response_var': None,
+        'formula': None,
+        'exp_params': None
+    }
+
+    # 需求分析
+    req = '我想要复现古巴导弹危机中美国和古巴在各个时间段的行为，分析什么因素对战争的走势影响最大'
+
     raAgent = RequirementAnalysisAgent(llm_model=MODEL_LIST[2])
     raObserver = RequirementAnalysisObserver(llm_model=MODEL_LIST[2])
-    req = '我想要复现古巴导弹危机中美国和古巴在各个时间段的行为，分析什么因素对战争的走势影响最大'
-    analysis_num = 1
+    analysis_num = 0
     while True:
-        analysis_res = raAgent.requirement_analysis(req)
-        print(analysis_res)
-        log_with_tag(message=json.dumps(analysis_res), tag='RA Result', level='info')
-        is_analysis_format_true = raObserver.requirement_format_judge(analysis_res)
+        ra_res = raAgent.requirement_analysis(req)
+        print(ra_res)
+        log_with_tag(message=json.dumps(ra_res), tag='RA Result', level='info')
+        # 检查需求分析结果格式是否正确
+        is_analysis_format_true = raObserver.requirement_format_judge(ra_res)
         if is_analysis_format_true:
             log_with_tag(message='需求解析成功', tag='RA Success', level='warning')
             print(f'------需求解析成功------')
+            exp_param['goal'] = ra_res['goal']
+            exp_param['influence_factor'] = ra_res['influence_factor']
+            exp_param['response_var'] = ra_res['response_var']
+            exp_param['formula'] = ra_res['formula']
+            exp_param['exp_params'] = ra_res['exp_params']
             break
         else:
             log_with_tag(message='需求解析失败', tag='RA Fail', level='error')
-            print(f'------需求解析错误({analysis_num})------')
+            print(f'------需求解析错误({++analysis_num})------')
 
+    # 影响因素和响应变量内容检查
     vcAgent = VariableControlAgent(llm_model=MODEL_LIST[2])
     vc_res = vcAgent.VCAnalysis(req=req,
-                                influence_factor=analysis_res['influence_factor'],
-                                response_var=analysis_res['response_var'],
-                                formula=analysis_res['formula'])
+                                influence_factor=ra_res['influence_factor'],
+                                response_var=ra_res['response_var'],
+                                formula=ra_res['formula'])
     print('VC Response: ', vc_res)
     if vc_res['is_reasonable'] == 1:
         log_with_tag(message='变量生成正确', tag='VC Success', level='warning')
     else:
         log_with_tag(message='变量生成错误，新变量如下', tag='VC Error', level='warning')
-
+        exp_param['influence_factor'] = vc_res['influence_factor']
+        exp_param['response_var'] = vc_res['response_var']
+        exp_param['formula'] = vc_res['formula']
     log_with_tag(message=json.dumps(vc_res), tag='VC Result', level='info')
+
+    # 实验参数检查
+    if vc_res['is_reasonable'] == 0:
+        vcAgent.VCExpParamAnalysis(req=req,
+                                   influence_factor=exp_param['influence_factor'],
+                                   response_var=exp_param['response_var'],
+                                   formula=exp_param['formula'],
+                                   exp_params=exp_param['exp_params'])
+
